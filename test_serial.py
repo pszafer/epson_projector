16b615172ea16f3b041aad0a21ca43d8594a5c85
Henryk Plötz <henryk@ploetzli.ch>
2019-10-03 22:00:14 +0200

    Change test program Enable more debugging, toggle power

--- test_serial.py
+++ test_serial.py
@@ -1,6 +1,6 @@
 import asyncio
 import epson_projector as epson
-from epson_projector.const import (POWER, PWR_ON)
+from epson_projector.const import (POWER, PWR_ON, PWR_OFF)
 import logging
 
 _LOGGER = logging.getLogger(__name__)
@@ -11,6 +11,7 @@ console_handler.setFormatter(logging.Formatter(
 _LOGGER.addHandler(console_handler)
 _LOGGER.setLevel(logging.DEBUG)
 
+logging.basicConfig(level=logging.DEBUG)
 
 async def main_serial(loop):
     """Run main with serial connection."""
@@ -18,13 +19,19 @@ async def main_serial(loop):
 
 
 async def run(loop):
-    projector = epson.Projector(host='/dev/ttyUSB1',
+    projector = epson.Projector(host='/dev/ttyUSB0',
                                 type='serial',
-                                loop=loop)
+                                loop=loop, timeout_scale=2.0)
     data = await projector.get_property(POWER)
     print(data)
-    data2 = await projector.send_command(PWR_ON)
-    print(data2)
+    cmd = None
+    if data == '01':
+        cmd = PWR_OFF
+    elif data == '00':
+        cmd = PWR_ON
+    if cmd:
+        data2 = await projector.send_command(cmd)
+        print(data2)
     projector.close()
 
 loop = asyncio.get_event_loop()
