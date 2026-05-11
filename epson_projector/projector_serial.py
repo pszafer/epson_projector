e83771455c6c4bf690431cc72a58bbef0eab20fa
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2026-05-11 20:56:43 +0200

    Use serialx

--- epson_projector/projector_serial.py
+++ epson_projector/projector_serial.py
@@ -2,7 +2,7 @@
 import logging
 
 import asyncio
-import serial_asyncio_fast
+import serialx
 from serial.serialutil import SerialException
 from .const import ESCVP_HELLO_COMMAND, COLON, CR, GET_CR, BUSY, ERROR, SNO
 from .base_connection import BaseProjectorConnection
@@ -46,7 +46,7 @@ class ProjectorSerial(BaseProjectorConnection):
                 (
                     self._reader,
                     self._writer,
-                ) = await serial_asyncio_fast.open_serial_connection(
+                ) = await serialx.open_serial_connection(
                     url=self._host, baudrate=9600, loop=self._loop
                 )
                 if self._reader and self._writer:
