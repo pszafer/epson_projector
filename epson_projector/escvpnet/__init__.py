e250515df7fb54708e8e682940e28e2236ebe4a0
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2026-05-24 19:23:04 +0200

    Sort exports

--- epson_projector/escvpnet/__init__.py
+++ epson_projector/escvpnet/__init__.py
@@ -6,25 +6,27 @@ https://www.epson.com.au/d/pub/epson/techtips/escvp.netmanual_e_f.pdf
 from __future__ import annotations
 
 from .error import (
-	BadRequestError,
-	BusyError,
-	PasswordRequiredError,
-	PasswordWrongError,
-	ProtocolVersionNotSupportedError,
-	RequestNotAllowedError,
-	UnknownStatusError,
+    BadRequestError,
+    BusyError,
+    EscVpNetException,
+    PasswordRequiredError,
+    PasswordWrongError,
+    ProtocolVersionNotSupportedError,
+    RequestNotAllowedError,
+    UnknownStatusError,
 )
 from .escvp21_communication import EscVp21Communication
 from .escvpnet import EscVpNet
 
 __all__ = [
-	"EscVpNet",
-	"EscVp21Communication",
-	"BadRequestError",
-	"PasswordRequiredError",
-	"PasswordWrongError",
-	"RequestNotAllowedError",
-	"BusyError",
-	"ProtocolVersionNotSupportedError",
-	"UnknownStatusError",
+    "BadRequestError",
+    "BusyError",
+    "EscVpNet",
+    "EscVp21Communication",
+    "EscVpNetException",
+    "PasswordRequiredError",
+    "PasswordWrongError",
+    "RequestNotAllowedError",
+    "ProtocolVersionNotSupportedError",
+    "UnknownStatusError",
 ]
