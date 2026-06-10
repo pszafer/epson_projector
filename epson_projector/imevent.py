5f8a97be72d2d34cfde827e363ed965e4db1f747
Michel van de Wetering <michel.van.de.wetering@gmail.com>
2026-06-10 21:42:46 +0200

    Initial IMEVENT handling

---
+++ epson_projector/imevent.py
@@ -0,0 +1,93 @@
+"""This module defines the IMEVENT message and related enums."""
+
+from __future__ import annotations
+
+from enum import IntEnum, IntFlag
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class ProjectorStatus(IntEnum):
+    STANDBY = 0x01
+    WARMUP = 0x02
+    NORMAL = 0x03
+    COOLDOWN = 0x04
+    ABNORMAL = 0xFF
+
+    _UNKNOWN = -1
+
+    @classmethod
+    def _missing_(cls, value: object) -> ProjectorStatus:
+        _LOGGER.warning("Unknown value '%s' for %s", value, cls.__name__)
+        return cls._UNKNOWN
+
+
+class WarningType(IntFlag):
+    LAMP_LIFE = 0x01
+    NO_SIGNAL = 0x02
+    UNSUPPORTED_SIGNAL = 0x04
+    AIR_FILTER = 0x08
+    HIGH_TEMPERATURE = 0x10
+    ASPECT_CHANGE = 0x40
+
+
+class AlarmType(IntFlag):
+    LAMP_ON_FAILURE = 0x01
+    LAMP_LID = 0x02
+    LAMP_BURNOUT = 0x04
+    FAN = 0x08
+    TEMPERATURE_SENSOR = 0x10
+    HIGH_TEMPERATURE = 0x20
+    INTERIOR_SYSTEM = 0x40
+
+
+class ImEvent:
+    def __init__(
+        self,
+        event_code: int,
+        power_status: ProjectorStatus,
+        warning_type: WarningType,
+        alarm_type: AlarmType,
+    ):
+        self.event_code = event_code
+        self.power_status = power_status
+        self.warning_type = warning_type
+        self.alarm_type = alarm_type
+
+    def __str__(self) -> str:
+        return f"ImEvent(event_code={self.event_code}, power_status={self.power_status}, warning_type={self.warning_type}, alarm_type={self.alarm_type})"
+
+    @staticmethod
+    def from_message(raw_message: bytes) -> ImEvent:
+        """Parse an IMEVENT message and return an ImEvent object."""
+
+        # Example message from LS11000
+        # b'IMEVENT=0001 03 00000002 00000000 T1 F1\r:'
+        message = raw_message.decode().rstrip("\r:")
+
+        (command, value) = message.split(sep="=", maxsplit=1)
+        if command != "IMEVENT":
+            raise ValueError(f"Command {command} is not IMEVENT")
+
+        parts = value.split()
+        if len(parts) < 4:
+            raise ValueError(
+                f"Expected at least eventcode and 3 parameters. Found {len(parts)} in message: {message}"
+            )
+
+        event_code = int(parts[0], 16)
+        if event_code != 1:
+            raise ValueError(f"Unknown event code: {event_code}")
+
+        # The parameters have 0 prefixes, so probably hex values.
+        power_status = ProjectorStatus(int(parts[1], 16))
+        warning_type = WarningType(int(parts[2], 16))
+        alarm_type = AlarmType(int(parts[3], 16))
+
+        return ImEvent(
+            event_code=event_code,
+            power_status=power_status,
+            warning_type=warning_type,
+            alarm_type=alarm_type,
+        )
--- epson_projector/projector_tcp.py
+++ epson_projector/projector_tcp.py
@@ -7,11 +7,13 @@ import asyncio
 
 from epson_projector.error import ProjectorUnavailableError, UnauthorizedError
 from epson_projector.escvpnet.error import EscVpNetConnectionError, EscVpNetForbiddenStatus, EscVpNetUnauthorizedStatus
-from epson_projector.escvpnet.escvpnet import EscVpNet
+from epson_projector.escvpnet.escvpnet import ESC_VPNET_PORT, EscVpNet
+from epson_projector.imevent import ImEvent
 
 from .base_connection import BaseProjectorConnection
 from .const import (
     BUSY,
+    COLON,
     ERROR,
     CR,
     CR_COLON,
@@ -26,24 +28,63 @@ from .timeout import get_timeout
 _LOGGER = logging.getLogger(__name__)
 
 
+class PendingCommand:
+    """Class to represent a pending command."""
+
+    def __init__(self, command: str):
+        self._command = command
+        self._future = asyncio.get_running_loop().create_future()
+
+    def cancel(self) -> None:
+        """Cancel the pending command."""
+        if not self._future.done():
+            self._future.cancel()
+
+    async def wait_for_response(self) -> bytes:
+        """Wait for the response of the command."""
+        return await self._future
+
+    def handle_response(self, response: bytes) -> bool:
+        """Handle response if reponse to this command. Return True if handled, False otherwise."""
+
+        is_response = False
+
+        if response == b"ERR\r:":
+            is_response = True
+        elif self._command.endswith("?\r"): # Get command
+            if response.startswith(f"{self._command[:-2]}=".encode()):
+                is_response = True
+        elif response == b":": # This is set or null command
+            is_response = True
+
+        if is_response and not self._future.done():
+            self._future.set_result(response)
+
+        return is_response
+        
+
 class ProjectorTcp(BaseProjectorConnection):
     """
     Epson TCP connector
     """
 
-    def __init__(self, host, port=3629, password=None):
+    def __init__(self, host, port=ESC_VPNET_PORT, password=None, on_imevent=None):
         """
         Epson TCP connector
 
         :param str host:     IP address of Projector
         :param int port:     Port to connect to. Default 3629.
         :param str password: Password for the projector. Default None.
+        :param callable on_imevent: Callback for IMEVENT messages. Default None.
         """
         self._host = host
         self._port = port
         self._password = password
+        self._on_imevent = on_imevent
         self._isOpen = False
         self._serial = None
+        self._listener_task = None
+        self._pending_command: PendingCommand | None = None
 
     async def async_init(self) -> None:
         """Async init to open connection with projector."""
@@ -51,6 +92,7 @@ class ProjectorTcp(BaseProjectorConnection):
             async with asyncio.timeout(10):
                 escvpnet = EscVpNet(host=self._host, port=self._port, password=self._password)
                 self._reader, self._writer = await escvpnet.connect()
+                self._listener_task = asyncio.create_task(self._listener_task_impl(self._reader, self._writer))
                 self._isOpen = True
                 _LOGGER.info("Connection open")
         except asyncio.TimeoutError:
@@ -64,10 +106,35 @@ class ProjectorTcp(BaseProjectorConnection):
         if self._isOpen:
             self._writer.close()
 
+    async def _listener_task_impl(self, reader, writer) -> None:
+        """Listener task for messages coming from the projector."""
+        try:
+            while not writer.is_closing():
+                raw_response = await reader.readuntil(COLON.encode())
+                _LOGGER.debug("Received: %s pending_command=%s)", raw_response, self._pending_command is not None)
+
+                if (cmd := self._pending_command) and cmd.handle_response(raw_response):
+                    continue
+    
+                # Must be an unsolicited message
+                if raw_response.startswith(b"IMEVENT="):
+                    _LOGGER.info("Received IMEVENT: %s", raw_response)
+                    if self._on_imevent:
+                        try:
+                            imevent = ImEvent.from_message(raw_response)
+                            self._on_imevent(imevent)
+                        except ValueError as e:
+                            _LOGGER.error("Error parsing IMEVENT: %s", e)
+                    continue
+
+                _LOGGER.warning("Received unexpected message: %s", raw_response)
+        except Exception as e:
+            _LOGGER.error("Error in listener task: %s", e)
+
     async def get_property(self, command, timeout, bytes_to_read=16) -> str | bool | int:
         """Get property state from device."""
         response = await self.send_request(
-            timeout=timeout, command=command + GET_CR, bytes_to_read=bytes_to_read
+            timeout=timeout, params=command + GET_CR, bytes_to_read=bytes_to_read
         )
         _LOGGER.debug("Response is %s", response)
         if not response:
@@ -86,23 +153,40 @@ class ProjectorTcp(BaseProjectorConnection):
 
     async def send_command(self, command, timeout) -> str | bool | None:
         """Send command to Epson."""
-        response = await self.send_request(timeout=timeout, command=command + CR)
+        response = await self.send_request(timeout=timeout, params=command + CR)
         return response
 
-    async def send_request(self, timeout, command, bytes_to_read=16) -> str | bool | None:
+    async def send_request(self, timeout, params, bytes_to_read=16) -> str | bool | None:
         """Send TCP request to Epson."""
         if self._isOpen is False:
             await self.async_init()
-        if self._isOpen and command:
+        if self._isOpen and params:
             bytes_to_read = bytes_to_read if bytes_to_read else 16
-            async with asyncio.timeout(timeout):
-                self._writer.write(command.encode())
-                await self._writer.drain()
-                response = await self._reader.read(bytes_to_read)
-                response = response.decode().replace(CR_COLON, "")
-                if response == ERROR:
-                    return False
-                return response
+            try:
+                async with asyncio.timeout(timeout):
+                    # Note that command has ?\r already appended
+                    pending_command = PendingCommand(params)
+                    self._pending_command = pending_command
+
+                    raw_command = params.encode()
+                    _LOGGER.debug("Sending: %s", raw_command)
+
+                    self._writer.write(raw_command)
+                    await self._writer.drain()
+
+                    # response = await self._reader.read(bytes_to_read)
+                    response = await pending_command.wait_for_response()
+                    response = response.decode().replace(CR_COLON, "")
+                    if response == ERROR:
+                        return False
+                    return response
+            except asyncio.TimeoutError as e:
+                _LOGGER.error("Timeout error receiving response for command %s", params)
+                if pc := self._pending_command:
+                    pc.cancel()
+                self._pending_command = None
+                # Raise again to keep current behavior
+                raise e
         return None
 
     async def get_serial_number(self) -> str | None:
